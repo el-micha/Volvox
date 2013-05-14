84b868d9b2ad13c148cb37a0e49129970e7e7832
Michael Weigelt <michael.weigelt@bluewin.ch>
2013-05-14 21:11:51 +0200

    Added path for predator.

--- src/main/Constants.java
+++ src/main/Constants.java
@@ -10,6 +10,7 @@ public interface Constants
 	public final int DELAY_MS = 30;
 	public final int DELAY_NS = 10;
 	
-	public final String IMG_PATH = "C:\\Dokumente und Einstellungen\\Michael\\Eigene Dateien\\GitHub\\Volvox\\src\\images\\";
+	//public final String IMG_PATH = "C:\\Dokumente und Einstellungen\\Michael\\Eigene Dateien\\GitHub\\Volvox\\src\\images\\";
+	public final String IMG_PATH = "D:\\GitHub\\Volvox\\src\\images\\";
 	
 }
