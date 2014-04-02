b5038ef34a5ada4849866a977f0ea7fc264f5693
el-micha <michael.weigelt@bluewin.ch>
2014-04-02 14:52:35 +0200

    X1 Setup
    Ain't nobody got time fo dat.

--- src/main/Constants.java
+++ src/main/Constants.java
@@ -5,7 +5,7 @@ public interface Constants
 	
 	public final int GAMEBOARD_WIDTH = 512 + 256 + 128;
 	public final int GAMEBOARD_HEIGHT = 512 + 128;
-	public final int CELL_SIZE = 6;
+	public final int CELL_SIZE = 8;
 	
 	public final int RULE_SURVIVE_MIN = 4;
 	public final int RULE_SURVIVE_MAX = 6;
@@ -21,19 +21,29 @@ public interface Constants
 	
 	public final boolean OUTPUT = true;
 	
-	/*	MACHINE DEPENDENT VARIABLES  */
+	/*	MACHINE DEPENDENT CONSTANTS  */
 	
 	
+	//Lenovo X1
+	public final String IMG_PATH = "C:\\Users\\Michael\\Documents\\GitHub\\Volvox\\src\\images\\";
+	public final int WINDOW_FRAME_OFFSET_X = 12;
+	public final int WINDOW_FRAME_OFFSET_Y = 44;
+	
+	/*
+	//HP
 	public final String IMG_PATH = "C:\\Users\\Michael\\Documents\\GitHub\\Volvox\\src\\images\\";
 	public final int WINDOW_FRAME_OFFSET_X = 8;
 	public final int WINDOW_FRAME_OFFSET_Y = 30;
+	*/
 	
 	/*
+	//Predator
 	public final String IMG_PATH = "D:\\GitHub\\Volvox\\src\\images\\";
 	public final int WINDOW_FRAME_OFFSET_X = 8;
 	public final int WINDOW_FRAME_OFFSET_Y = 30;
 	*/
 	/*
+	//MacBook
 	public final String IMG_PATH = "/Users/suzanneweigelt/Documents/Michael/eclipse/Volvox/src/images/";
 	public final int WINDOW_FRAME_OFFSET_X = 2;
 	public final int WINDOW_FRAME_OFFSET_Y = 22;
